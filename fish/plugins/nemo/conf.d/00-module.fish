996295870d1824e668ec96ea712d71bc4e45396f
Prasad Tengse <tprasadtp@users.noreply.github.com>
2021-06-02 18:12:33 +0200

    [AUTO] luna:2.1.5:HLC:k8s:dotfiles-updater:550fada8ce49

--- fish/plugins/nemo/conf.d/00-module.fish
+++ fish/plugins/nemo/conf.d/00-module.fish
@@ -1,6 +1,6 @@
 # P0.Dependent: 99-preload-modules.fish
 # https://lmod.readthedocs.io/en/latest/030_installing.html
 # on NEMO lmod is installed to /usr/share/lmod
-if test -f $HOME/opt/fish/bin
+if test -f /usr/share/lmod/lmod/init/profile.fish
   source /usr/share/lmod/lmod/init/profile.fish
 end
